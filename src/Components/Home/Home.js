22b8b3628c013b8d0a308caffefeeefe33956780
Prakhar Kumar Srivastava <prakhar25789@gmail.com>
2023-04-27 05:19:16 +0530

    Textual Editing

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,11 +6,11 @@ function Home() {
     <div className='container'>
         <div className="allButtons">
         <Link to="/assignment/0"><button type="button" class="m-2 btn btn-lg btn-primary">All Data</button></Link>
-        <Link to="/assignment/1"><button type="button" class="m-2 btn btn-lg btn-info"> Assignment 1</button></Link>
-        <Link to="/assignment/2"><button type="button" class="m-2 btn btn-lg btn-dark"> Assignment 2</button></Link>
-        <Link to="/assignment/3"><button type="button" class="m-2 btn btn-lg btn-primary"> Assignment 3</button></Link>
-        <Link to="/assignment/4"><button type="button" class="m-2 btn btn-lg btn-info"> Assignment 4</button></Link>
-        <Link to="/assignment/5"><button type="button" class="m-2 btn btn-lg btn-dark"> Assignment 5</button></Link>
+        <Link to="/assignment/1"><button type="button" class="m-2 btn btn-lg btn-info"> Assignment Answer 1</button></Link>
+        <Link to="/assignment/2"><button type="button" class="m-2 btn btn-lg btn-dark"> Assignment Answer 2</button></Link>
+        <Link to="/assignment/3"><button type="button" class="m-2 btn btn-lg btn-primary"> Assignment Answer 3</button></Link>
+        <Link to="/assignment/4"><button type="button" class="m-2 btn btn-lg btn-info"> Assignment Answer 4</button></Link>
+        <Link to="/assignment/5"><button type="button" class="m-2 btn btn-lg btn-dark"> Assignment Answer 5</button></Link>
         </div>
     </div>
   )
